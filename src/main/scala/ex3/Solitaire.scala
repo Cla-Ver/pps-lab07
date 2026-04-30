2d0aed140f502593f022b28640215b10c47b0d03
Claudio Verazza <claudio.verazza@studio.unibo.it>
2026-04-30 11:15:29 +0200

    Done Solitaire

--- src/main/scala/ex3/Solitaire.scala
+++ src/main/scala/ex3/Solitaire.scala
@@ -11,14 +11,45 @@ object Solitaire extends App:
       yield row.mkString
     rows.mkString("\n")
 
-  type Solution = Iterable[Int]
+  type Pos = (Int, Int)
+  type Solution = Iterable[Pos]
   type IterableFactory = Solution => Iterable[Solution]
   given IterableFactory = LazyList(_)
 
-  /*def placeMarks(boardSize: Int): Iterable[Solution] =
-    for
-      x <- 0 to boardSize
-      y <- 0 to boardSize
-      numberPos = (x, y)*/
+  private val MINIMUM_BOARD_SIZE = 5
 
-  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+  def placeMarks(boardSize: Int)(using factory: IterableFactory): Iterable[Solution] =
+    def placeMarksHelper(solutionUntilNow: Solution): Iterable[Solution] = boardSize match
+      case n if n < MINIMUM_BOARD_SIZE || solutionUntilNow.size == boardSize * boardSize => factory(solutionUntilNow)
+      case _ =>
+        for
+          x <- 0 until boardSize
+          y <- 0 until boardSize
+          numberPos = (x, y)
+          if isPositionFree(numberPos, solutionUntilNow)
+          if isValidPosition(numberPos, solutionUntilNow)
+          solution <- placeMarksHelper(numberPos +: solutionUntilNow.toSeq) //placeholder
+        yield
+          solution
+
+    placeMarksHelper(List((boardSize / 2, boardSize / 2)))
+
+  private def isValidPosition(position: (Int, Int), others: Iterable[Pos]): Boolean = if others.isEmpty then true
+     else
+      others.head match
+        case (prevRow, prevCol) =>
+          val rowDiff = math.abs(position._1 - prevRow)
+          val colDiff = math.abs(position._2 - prevCol)
+          (rowDiff == 3 && colDiff == 0) || (rowDiff == 0 && colDiff == 3) || (rowDiff == 2 && colDiff == 2)
+
+
+  private def isPositionFree(position: (Int, Int), others: Iterable[Pos]) = !others.toSeq.contains(position)
+
+
+  //placeMarks(5)
+  placeMarks(5).foreach(s =>
+    println(render(s.toSeq, width = 5, height = 5))
+    println("----------")
+  )
+  //println(render(placeMarks(5).head.toSeq, width = 5, height = 5))
+  //println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
